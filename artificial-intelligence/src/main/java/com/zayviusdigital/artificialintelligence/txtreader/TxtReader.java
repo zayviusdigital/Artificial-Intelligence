d89923cbff1b53ff8e1833fdb7c37dd69985112f
Zayvius Digital <74743971+zayviusdigital@users.noreply.github.com>
2023-01-02 21:24:46 +0700

    First Release

---
+++ artificial-intelligence/src/main/java/com/zayviusdigital/artificialintelligence/txtreader/TxtReader.java
@@ -0,0 +1,35 @@
+package com.zayviusdigital.artificialintelligence.txtreader;
+
+import android.content.Context;
+
+import com.zayviusdigital.artificialintelligence.ArtificialIntelligence;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+public class TxtReader {
+    public static String set(Context context,String txt_source){
+        InputStream inputStream = null;
+        try {
+            inputStream = context.getAssets().open(txt_source);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int size = 0;
+        try {
+            size = Objects.requireNonNull(inputStream).available();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        byte[] buffer = new byte[size];
+        try {
+            if (inputStream != null) {
+                int read = inputStream.read(buffer);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ArtificialIntelligence.set_keys(new String(buffer));
+    }
+}

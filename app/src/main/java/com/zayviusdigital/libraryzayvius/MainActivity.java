11b7c6e6abb110b308f871831439a4c9d18680f8
Zayvius Digital <74743971+zayviusdigital@users.noreply.github.com>
2023-01-02 21:10:47 +0700

    First Release

--- app/src/main/java/com/zayviusdigital/libraryzayvius/MainActivity.java
+++ app/src/main/java/com/zayviusdigital/libraryzayvius/MainActivity.java
@@ -3,16 +3,11 @@ package com.zayviusdigital.libraryzayvius;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.zayviusdigital.artificialintelligence.ArtificialIntelligence;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("hash_key",  ArtificialIntelligence.set_keys(""));
     }
 }
